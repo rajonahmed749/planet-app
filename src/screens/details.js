8c92832e47d8dab79f6fb10b444cef49bc050193
rajonahmed749 <raazon749@gmail.com>
2022-05-24 21:00:38 +0600

    Every thing done

--- src/screens/details.js
+++ src/screens/details.js
@@ -1,52 +1,78 @@
 import React from 'react'
-import { View, StyleSheet, ScrollView, Image ,Pressable} from 'react-native';
+import { View, StyleSheet, ScrollView, Image, Pressable, Linking } from 'react-native';
 import PlanetHeader from '../components/planet-header'
 import Text from '../components/text/text'
 import { earthPic, jupiterPic, marsPic, mercuryPic, neptunePic, saturnPic, uranusPic, venusPic } from '../svg';
 import { colors } from '../theme/colors'
 import { spacing } from '../theme/spacing';
 
-const Details = ({ navigation, route }) => {
-    const planet = route.params.planet;
-    console.log("THe planet list =--> ", planet);
-    const { name, description } = planet;
 
-    // const renderImage = (name) => {
-    //     switch (name) {
-    //         case 'mercury':
-    //             return < Image style={styles.planetImage} source={require(venusPic)} />;
-    //         case 'venus':
-    //             return < Image style={styles.planetImage} source={require(venusPic)} />;
-    //         case 'earth':
-    //             return < Image style={styles.planetImage} source={require(earthPic)} />;
-    //         case 'mars':
-    //             return < Image style={styles.planetImage} source={require(marsPic)} />;
-    //         case 'jupiter':
-    //             return < Image style={styles.planetImage} source={require(jupiterPic)} />;
-    //         case 'saturn':
-    //             return < Image style={styles.planetImage} source={require(saturnPic)} />;
-    //         case 'uranus':
-    //             return < Image style={styles.planetImage} source={require(uranusPic)} />;
-    //         case 'neptune':
-    //             return < Image style={styles.planetImage} source={require(neptunePic)} />;
-    //     }
-    // }
+const PlanetSection = ({ title, value }) => {
+    return (
+        <View style={styles.planetSection}>
+            <Text preset='small' style={{ textTranform: 'uppercase' }}>{title}</Text>
+            <Text preset='h3' >{value}</Text>
+        </View>
+    )
+}
+
+
+
+const Details = ({ route }) => {
+    const planet = route.params.planet;
+    const { name, description, rotationTime, revolutionTime, radius, avgTemp, wikiLink } = planet;
+
+    const RenderImage = () => {
+        switch (name) {
+            case 'mercury':
+                return < Image style={styles.planetImg} source={require('../svg/mercury.png')} />;
+            case 'venus':
+                return < Image style={styles.planetImg} source={require('../svg/venus.png')} />;
+            case 'earth':
+                return < Image style={styles.planetImg} source={require('../svg/earth.png')} />;
+            case 'mars':
+                return < Image style={styles.planetImg} source={require('../svg/mars.png')} />;
+            case 'jupiter':
+                return < Image style={styles.planetImg} source={require('../svg/jupiter.png')} />;
+            case 'saturn':
+                return < Image style={styles.planetImg} source={require('../svg/saturn.png')} />;
+            case 'uranus':
+                return < Image style={styles.planetImg} source={require('../svg/uranus.png')} />;
+            case 'neptune':
+                return < Image style={styles.planetImg} source={require('../svg/neptune.png')} />;
+        }
+    }
+
+    const onPressLink = () => {
+        Linking.openURL(wikiLink)
+    }
     return (
         <View style={styles.container}>
             <PlanetHeader BackBtn={true} />
             <ScrollView>
                 <View style={styles.imageView}>
-                    {/* {renderImage(name)} */}
-                    <Text preset='h3'> There will be an image for -- {name.toUpperCase()}</Text>
+                    {RenderImage(name)}
+                    {/* jodi ami svg gula ke component a convert kortam */}
+                    {/* <Image
+                        style={styles.planetImg}
+                        source={require('../svg/earth.png')}
+                    /> */}
                 </View>
+
                 <View style={styles.detailsView}>
                     <Text preset='h1' style={styles.name}> {name}</Text>
                     <Text preset='small' style={styles.description}> {description}</Text>
-                    <Pressable style={styles.source}>
+                    <Pressable style={styles.source} onPress={onPressLink}>
                         <Text preset='small'>Source: </Text>
                         <Text preset='h4' style={styles.wikipedia}>Wikipedia</Text>
                     </Pressable>
                 </View>
+
+                <View style={{ height: 40 }} />
+                <PlanetSection title='ROTATION TIME' value={rotationTime} />
+                <PlanetSection title='REVOLUTION TIME' value={revolutionTime} />
+                <PlanetSection title='RADIUS' value={radius} />
+                <PlanetSection title='AVG TEMP.' value={avgTemp} />
             </ScrollView>
         </View>
     )
@@ -73,18 +99,33 @@ const styles = StyleSheet.create({
     name: {
         textTransform: 'uppercase'
     },
-    description:{
-        marginTop:spacing[5],
-        textAlign:'center',
+    description: {
+        marginTop: spacing[5],
+        textAlign: 'center',
         lineHeight: 25
     },
-    source:{
-        flexDirection:'row',
-        alignItems:'center',
+    source: {
+        flexDirection: 'row',
+        alignItems: 'center',
         marginTop: spacing[4]
     },
-    wikipedia:{
+    wikipedia: {
         textDecorationLine: 'underline',
-        fontWeight:'bold'
+        fontWeight: 'bold'
+    },
+    planetSection: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingHorizontal: spacing[5],
+        paddingVertical: spacing[4],
+        borderWidth: 1,
+        borderColor: colors.darkGrey,
+        marginHorizontal: spacing[6],
+        marginBottom: spacing[4],
+    },
+    planetImg: {
+        height: 200,
+        width: 200
     }
 })
--- src/screens/home.js
+++ src/screens/home.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, StyleSheet, FlatList, Pressable } from 'react-native'
+import React, { useState } from 'react';
+import { View, StyleSheet, FlatList, Pressable, TextInput } from 'react-native'
 import PlanetHeader from '../components/planet-header';
 import Text from '../components/text/text';
 import { PLANET_LIST } from '../data/planet-list';
@@ -8,12 +8,33 @@ import { spacing } from '../theme/spacing';
 import { AntDesign } from '@expo/vector-icons';
 
 const Home = ({ navigation }) => {
+    const [list, setList] = useState(PLANET_LIST)
+
+    // search input gula input kortesi ei function er maddhome 
+    const searchFilter = (text) => {
+        const filterdList = PLANET_LIST.filter((item) => {
+            const itemName = item.name.toLowerCase();
+            const userTypedText = text.toLowerCase();
+            return itemName.indexOf(userTypedText) > -1;
+        });
+        setList(filterdList)
+        console.log('filterd list', filterdList)
+    }
     return (
         <View style={styles.container}>
             <PlanetHeader />
+
+            <TextInput
+                placeholder='Type the planet name'
+                placeholderTextColor={colors.white}
+                autoCorrect={false}
+                style={styles.searchInput}
+                onChangeText={(text) => { searchFilter(text) }}
+            />
+
             <FlatList
                 contentContainerStyle={styles.list}
-                data={PLANET_LIST} // puro array ta pacchi data er moddhe
+                data={list} // puro array ta pacchi data er moddhe
                 keyExtractor={(item, index) => item.name} // unique key 
                 renderItem={({ item, index }) => {
                     const { name, color } = item;
@@ -42,7 +63,8 @@ export default Home
 const styles = StyleSheet.create({
     container: {
         felx: 1,
-        backgroundColor: colors.black
+        backgroundColor: colors.black,
+        height: 900
     },
     list: {
         padding: spacing[4]
@@ -65,5 +87,12 @@ const styles = StyleSheet.create({
     sepaator: {
         borderBottomWidth: 0.5,
         borderBottomColor: colors.white
+    },
+    searchInput: {
+        padding: spacing[4],
+        color: colors.white,
+        borderBottomColor: colors.white,
+        borderBottomWidth: 1,
+        margin: spacing[5]
     }
 })
